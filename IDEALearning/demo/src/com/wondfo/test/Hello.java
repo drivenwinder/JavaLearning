fe28f84cc7507880d0b366ac100f0bb8d8d892a3
drivenwinder <wdhwyb@gmail.com>
2019-12-06 10:08:23 +0800

    add to git

--- IDEALearning/demo/src/com/wondfo/test/Hello.java
+++ IDEALearning/demo/src/com/wondfo/test/Hello.java
@@ -3,5 +3,6 @@ package com.wondfo.test;
 public class Hello {
     public  static  void main(String[] args){
         System.out.print("hello,IDEA");
+        System.out.print("add to git");
     }
 }
